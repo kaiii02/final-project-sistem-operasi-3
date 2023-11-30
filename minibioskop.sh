819f299177aa5e55d6d2969785371f45ea0427ae
Kai <kai@Kai.myguest.virtualbox.org>
2023-11-30 16:24:06 +0700

    updated

--- minibioskop.sh
+++ minibioskop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Program Mini Bioskop (POV : Cashier)
+#Program Mini Bioskop
 
 PILIH=1
 MOVIE=1
@@ -10,10 +10,15 @@ harga_bayar=0
 jumlah=0
 price(){
 	jumlah=$(dialog --inputbox "Masukkan jumlah tiket : " 15 50 3>&1 1>&2 2>&3)
-	if [ $jumlah -eq 0 ]; then
-		dialog --msgbox "Jumlah tidak valid" 15 50
+	response=$?
+	if [ $response -eq 0 ]; then
+		if [ $jumlah -eq 0 ]; then
+			dialog --msgbox "Jumlah tidak valid" 15 50
+		else
+			let "harga_bayar= $jumlah * $harga_tiket"
+		fi
 	else
-		let "harga_bayar= $jumlah * $harga_tiket"
+		break
 	fi
 }
 
@@ -61,6 +66,8 @@ movie_list(){
 				;;
 
 		esac
+	else
+		break
 	fi
 }
 
@@ -69,7 +76,17 @@ pnumber=""
 number=""
 identity(){
 	nama=$(dialog --inputbox "Masukkan nama : " 15 50 3>&1 1>&2 2>&3)
-	pnumber=$(dialog --inputbox "Masukkan no handphone : " 15 50 3>&1 1>&2 2>&3)
+	response=$?
+	if [ $response -eq 0 ]; then
+		pnumber=$(dialog --inputbox "Masukkan no handphone : " 15 50 3>&1 1>&2 2>&3)
+		if [ $response -eq 0 ]; then
+			movie_list
+		else
+			break
+		fi
+	else
+		break
+	fi
 }
 
 star=$(echo -e "\u2764\uFE0F")
@@ -81,74 +98,78 @@ four="$star $star $star $star"
 five="$star $star $star $star $star"
 starrating(){
 	notiket=$(dialog --inputbox "Masukkan no tiket : " 15 50 3>&1 1>&2 2>&3)
-	hasil=$(grep "$notiket" database.txt)
-	if [ -n "$hasil" ]; then
-		exec 3>&1
-		rating=$(dialog --title "RATING FILM" --menu "Seberapa love sih kamu : " 15 50 5 \
-		1 "$star" 2 "$star $star" 3 "$star $star $star" \
-		4 "$star $star $star $star" 5 "$star $star $star $star $star" 2>&1 1>&3)
-		exec 3>&-
-		case $rating in
-			1)
-				rate="$one"
-				tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
-				mrate=$(grep "$notiket" database.txt | cut -f4,6)
-				echo "$tanggal1	$mrate	$rate" >> movierate.txt
-				clear
-				dialog --msgbox "Terimakasih sudah memberi rating" 15 50
-				echo " "
-				;;
-			2)
-				rate="$two"
-				tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
- 				mrate=$(grep "$notiket" database.txt | cut -f4,6)
-				echo "$tanggal1	$mrate	$rate" >> movierate.txt
-				clear
-				dialog --msgbox "Terimakasih sudah memberi rating" 15 50
-				echo " "
-				;;
-			3)
-				rate="$three"
-				tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
- 				mrate=$(grep "$notiket" database.txt | cut -f4,6)
-				echo "$tanggal1	$mrate	$rate" >> movierate.txt
-				clear
-				dialog --msgbox "Terimakasih sudah memberi rating" 15 50
-				echo " "
-				;;
-			4)
-				rate="$four"
-				tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
- 				mrate=$(grep "$notiket" database.txt | cut -f4,6)
-				echo "$tanggal1	$mrate	$rate" >> movierate.txt
-				clear
-				dialog --msgbox "Terimakasih sudah memberi rating" 15 50
-				echo " "
-				;;
-			5)
-				rate="$five"
-				tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
- 				mrate=$(grep "$notiket" database.txt | cut -f4,6)
-				echo "$tanggal1	$mrate	$rate" >> movierate.txt
-				clear
-				dialog --msgbox "Terimakasih sudah memberi rating" 15 50
-				echo " "
-				;;
-			*)
-				clear
-				dialog --msgbox "Kamu belum memberikan rating" 15 50
-				main
-				;;
-		esac
+	response=$?
+	if [ $response -eq 0 ]; then
+		hasil=$(grep "$notiket" database.txt)
+		if [ -n "$hasil" ]; then
+			exec 3>&1
+			rating=$(dialog --title "RATING FILM" --menu "Seberapa love sih kamu : " 15 50 5 \
+			1 "$star" 2 "$star $star" 3 "$star $star $star" \
+			4 "$star $star $star $star" 5 "$star $star $star $star $star" 2>&1 1>&3)
+			exec 3>&-
+			case $rating in
+				1)
+					rate="$one"
+					tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
+					mrate=$(grep "$notiket" database.txt | cut -f4,6)
+					echo "$tanggal1	$mrate	$rate" >> movierate.txt
+					clear
+					dialog --msgbox "Terimakasih sudah memberi rating" 15 50
+					echo " "
+					;;
+				2)
+					rate="$two"
+					tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
+ 					mrate=$(grep "$notiket" database.txt | cut -f4,6)
+					echo "$tanggal1	$mrate	$rate" >> movierate.txt
+					clear
+					dialog --msgbox "Terimakasih sudah memberi rating" 15 50
+					echo " "
+					;;
+				3)
+					rate="$three"
+					tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
+ 					mrate=$(grep "$notiket" database.txt | cut -f4,6)
+					echo "$tanggal1	$mrate	$rate" >> movierate.txt
+					clear
+					dialog --msgbox "Terimakasih sudah memberi rating" 15 50
+					echo " "
+					;;
+				4)
+					rate="$four"
+					tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
+ 					mrate=$(grep "$notiket" database.txt | cut -f4,6)
+					echo "$tanggal1	$mrate	$rate" >> movierate.txt
+					clear
+					dialog --msgbox "Terimakasih sudah memberi rating" 15 50
+					echo " "
+					;;
+				5)
+					rate="$five"
+					tanggal1=$(date +"%d-%m-%Y %H:%M:%S")
+ 					mrate=$(grep "$notiket" database.txt | cut -f4,6)
+					echo "$tanggal1	$mrate	$rate" >> movierate.txt
+					clear
+					dialog --msgbox "Terimakasih sudah memberi rating" 15 50
+					echo " "
+					;;
+					*)
+					clear
+					dialog --msgbox "Kamu belum memberikan rating" 15 50
+					main
+					;;
+			esac
+		else
+			dialog --msgbox "silahkan lakukan pemesanan tiket terlebih dahulu" 15 50
+		fi
 	else
-		dialog --msgbox "silahkan lakukan pemesanan tiket terlebih dahulu" 15 50
+		break
 	fi
 
 }
 
 tanggal=$(date +"%d-%m-%Y	%H:%M:%S")
 tiket(){
-	echo " "
 	echo "--------- TIKET MINIBIOSKOP ---------"
 	echo "TANGGAL : $tanggal"
 	id=$(date +\%m\%S\%d\%M)
@@ -159,26 +180,40 @@ tiket(){
 	echo "$tanggal	$pnumber	$id	$nama	$judul	$jumlah	$harga_bayar" >> database.txt
 	echo " "
 	echo "Terimakasih Kak $nama"
-	echo "!!!TUKARKAN TIKET MAX H+7 DARI TANGGAL PEMBELIAN!!!"
+	echo " "
+	echo "!!! DISCLAIMER !!!"
+	echo "SIMPAN NO TIKET UNTUK DITUNJUKKAN SAAT PEMBAYARAN DAN PENUKARAN TIKET FISIK"
+	echo "PEMBAYARAN DAN PENUKARAN TIKET HANYA BISA DILAKUKAN OFFLINE DI VENUE ACARA"
+	echo "!!!TIKET BERLAKU HINGGA H+7 DARI TANGGAL PEMESANAN!!!"
 }
 
 order(){
 	identity
-	movie_list
-	printtiket=$(tiket)
-	dialog --msgbox "$printtiket" 15 50
+	dialog --yesno "Apakah Anda yakin ingin melanjutkan pembelian?"	15 50
+	response=$?
+	if [ $response = 0 ]; then
+		printtiket=$(tiket)
+		dialog --msgbox "$printtiket" 50 80
+	else
+		break
+	fi
 	clear
 }
 
 checkIn(){
 	idtiket=$(dialog --inputbox "Masukkan no tiket : " 15 50 3>&1 1>&2 2>&3)
-	hasil=$(grep "$idtiket" database.txt)
-	if [ -n "$hasil" ]; then
-		dialog --msgbox "SELAMAT MENIKMATI FILM DI MINIBIOSKOP" 15 50
+	response=$?
+	if [ $response -eq 0 ]; then
+		hasil=$(grep "$idtiket" database.txt)
+		if [ -n "$hasil" ]; then
+			dialog --msgbox "SELAMAT MENIKMATI FILM DI MINIBIOSKOP" 15 50
+		else
+			dialog --msgbox "!!!DATA TIDAK DITEMUKAN!!!" 15 50
+			clear
+			pr -h "DATABASE TICKET" database.txt | more
+		fi
 	else
-		dialog --msgbox "!!!DATA TIDAK DITEMUKAN!!!" 15 50
-		clear
-		pr -h "DATABASE TICKET" database.txt | more
+		break
 	fi
 }
 
@@ -208,24 +243,29 @@ pelanggan(){
 
 pegawai(){
 	pass=$(dialog --inputbox "Masukkan password : " 15 50 3>&1 1>&2 2>&3)
-	if [ "$pass" == "87654321" ]; then
-		exec 3>&1
-		pilih2=$(dialog --title "SELAMAT DATANG DI HALAMAN MINI BIOSKOP" --menu " " 15 50 4 \
-		1 "Pesan Tiket Nonton" 2 "Check In" 3 "Lihat Database Tiket" 4 "Info Rating" 2>&1 1>&3)
-		exec 3>&-
-		case $pilih2 in
-			1)	order ;;
-			2)	checkIn ;;
-			3)	databaseTiket ;;
-			4)	infoRating ;;
-			*)	echo "Pilihan tidak valid" ;;
-		esac
+	response=$?
+	if [ $response -eq 0 ]; then
+		if [ "$pass" == "87654321" ]; then
+			exec 3>&1
+			pilih2=$(dialog --title "SELAMAT DATANG DI HALAMAN MINI BIOSKOP" --menu " " 15 50 4 \
+			1 "Pesan Tiket Nonton" 2 "Check In" 3 "Lihat Database Tiket" 4 "Info Rating" 2>&1 1>&3)
+			exec 3>&-
+			case $pilih2 in
+				1)	order ;;
+				2)	checkIn ;;
+				3)	databaseTiket ;;
+				4)	infoRating ;;
+				*)	echo "Pilihan tidak valid" ;;
+			esac
+			clear
+		else
+			dialog --msgbox "Password salah" 15 50
+			main
+		fi
 		clear
 	else
-		dialog --msgbox "Password salah" 15 50
-		main
+		break
 	fi
-	clear
 }
 
 main(){
